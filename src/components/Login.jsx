99ccde34b1bd8ef0d221385dafcf4b2dbc294838
Yuuki Suzuki <07yuki1101@gmail.com>
2026-02-15 15:24:17 +0900

    ログインボタンにアイコンを挿入

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import { auth } from "../firebase";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
+import googleIcon from "../assets/google-brand-color.svg";
 function Login() {
   const handleLogin = async () => {
     try {
@@ -14,9 +14,7 @@ function Login() {
   return (
     <div style={{ textAlign: "center", marginTop: "40px" }}>
       <h2>ログイン</h2>
-      <button onClick={handleLogin}>
-        Googleでログイン
-      </button>
+      <button className="login-btn" onClick={handleLogin}><img className="login-icon" src={googleIcon} alt="" /> でログイン</button>
     </div>
   );
 }
